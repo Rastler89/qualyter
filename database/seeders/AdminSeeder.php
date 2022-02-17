fc3e18b0863f80c046a3c8793cff238fb6876632
Dani <daniel.molina@optimaretail.es>
2022-02-17 18:21:44 +0100

    process

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,17 +11,22 @@ class AdminSeeder extends Seeder {
 
     public function run() {
         DB::table('permissions')->insert([
-            'slug' => 'view-roles',
-            'name' => 'Ver roles'
-        ], [
-            'slug' => 'create-roles',
-            'name' => 'Crear roles'
-        ], [
-            'slug' => 'edit-roles',
-            'name' => 'Modificar roles'
-        ], [
-            'slug' => 'assign-roles',
-            'name' => 'Assignar rol'
+            [
+                'slug' => 'view-roles',
+                'name' => 'Ver roles'
+            ], [
+                'slug' => 'create-roles',
+                'name' => 'Crear roles'
+            ], [
+                'slug' => 'edit-roles',
+                'name' => 'Modificar roles'
+            ], [
+                'slug' => 'assign-roles',
+                'name' => 'Assignar rol'
+            ], [
+                'slug' => 'edit-users',
+                'name' => 'Modificar Usuarios'
+            ]
         ]);
 
         DB::table('roles')->insert([
@@ -30,30 +35,43 @@ class AdminSeeder extends Seeder {
         ]);
 
         DB::table('users')->insert([
-            'name' => 'Dani Molina',
-            'email' => 'daniel.molina@optimaretail.es',
-            'password' => Hash::make('123456')
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Banejat',
-            'email' => 'banned@test.es',
-            'password' => Hash::make('123456'),
-            'status' => 0
+            [
+                'name' => 'Dani Molina',
+                'email' => 'daniel.molina@optimaretail.es',
+                'password' => Hash::make('123456'),
+                'status' => 1
+            ], [
+                'name' => 'Banejat',
+                'email' => 'banned@test.es',
+                'password' => Hash::make('123456'),
+                'status' => 0
+            ]
         ]);
 
         DB::table('roles_permissions')->insert([
-            'role_id' => 1,
-            'permission_id' => 1
-        ], [
-            'role_id' => 1,
-            'permission_id' => 2
-        ], [
-            'role_id' => 1,
-            'permission_id' => 3
-        ], [
-            'role_id' => 1,
-            'permission_id' => 4
+            [
+                'role_id' => 1,
+                'permission_id' => 1
+            ], [
+                'role_id' => 1,
+                'permission_id' => 2
+            ], [
+                'role_id' => 1,
+                'permission_id' => 3
+            ], [
+                'role_id' => 1,
+                'permission_id' => 4
+            ], [
+                'role_id' => 1,
+                'permission_id' => 5
+            ]
+        ]);
+
+        DB::table('users_roles')->insert([
+            [
+                'role_id' => 1,
+                'user_id' => 1
+            ]
         ]);
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,13 +4,19 @@
 <table class="table table-hover table-striped">
     <thead>
         <tr>
-            <th scope="col">Name</th>
+            <th scope="col">{{__('Name')}}</th>
+            <th scope="col">{{__('Status')}}</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($users as $user)
         <tr>
             <td>{{$user->name}}</td>
+            <td>
+                @can('edit-users')
+                    hola
+                @endcan
+            </td>
         </tr>
         @endforeach
     </tbody>
