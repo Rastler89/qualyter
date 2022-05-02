b19c602e29bc98f2148b0874d418f7f0c05b6143
Daniel Molina <daniel.molina@optimaretail.es>
2022-05-02 11:22:22 +0200

    process

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Answer;
+
+
 class ExportController extends Controller
 {
     public function answer(Request $request) {
@@ -12,6 +15,36 @@ class ExportController extends Controller
             'end_date' => 'required', 
         ]);
 
+        $headers = [
+            'Cache-Control'         => 'must-revalidate, post-check=0, pre-check=0',
+            'Content-type'          => 'text/csv',
+            'Content-Disposition'   => 'attachment; filename=answers.csv',
+            'Expires'               => '0',
+            'Pragma'                => 'public'
+        ];
+
+        $list = Answer::whereBetween('expiration', [$request->get('start_date'), $request->get('end_date')])->whereIn('status',array(2,4,5));
         
+        if(!is_null($request->get('client')) && $request->get('client') != '') {
+            $list->where('client','=',$request->get('client'));
+        }
+        if(!is_null($request->get('store')) && $request->get('store') != '') {
+            $list->where('store','=',$request->get('store'));
+        }
+        
+        $list = $list->get()->toArray();
+        
+        array_unshift($list, array_keys($list[0]));
+
+        $callback = function() use ($list) 
+        {
+            $FH = fopen('php://output', 'w');
+            foreach ($list as $row) { 
+                fputcsv($FH, $row);
+            }
+            fclose($FH);
+        };
+
+        return response()->stream($callback, 200, $headers);
     }
 }
--- resources/views/admin/answer/index.blade.php
+++ resources/views/admin/answer/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard')
 
-@section('content')<!--
+@section('content')
 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
   <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#downloadCSV"><i class="align-middle" data-feather="download"></i></button>
 </div>
@@ -11,7 +11,7 @@
         <h5 class="modal-title" id="staticBackdropLabel">{{__('Export CSV')}}</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <form method="POST" action="">
+      <form method="POST" action="{{route('export.answer')}}">
           @csrf
       <div class="modal-body">
         <label>{{__('Search among')}}:</label>
@@ -38,24 +38,15 @@
                 @endforeach
             </datalist>
         </div>
-        <div class="mb-3">
-            <label for="agent" class="form-label">{{__('Agent')}}</label>
-            <input class="form-control" list="agents" id="agent" name="agent" placeholder="{{__('Type to search...')}}" />
-            <datalist id="agents">
-                @foreach($agents as $agent)  
-                    <option value="{{$agent->id}}">{{$agent->name}}</option>
-                @endforeach
-            </datalist>
-        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
-        <button type="button" class="btn btn-success">{{__('Export')}}</button>
+        <button class="btn btn-success">{{__('Export')}}</button>
       </div>
 </form>
     </div>
   </div>
-</div>-->
+</div>
 <table class="table table-hover table-striped">
     <thead>
         <tr>
