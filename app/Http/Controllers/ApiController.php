288c71513f5b7688d10fe90d2d61924a70809008
Daniel Molina <daniel.molina@optimaretail.es>
2022-06-20 09:59:09 +0200

    process

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,30 +10,44 @@ class ApiController extends Controller
     public function emails(Request $request) {
         $body = json_decode($request->getContent(), true);
         
-        $answer = Answer::whereIn('status',[3,4]);
+        $answers = Answer::whereIn('status',[3,4]);
 
+        $answers = $this->dating($body,$answers);
+
+        $answers = $answers->get();
+
+        $res = [];
+        $res['total'] = count($answers);
+
+        if($body['not_respond']) {
+            $responds = Answer::where('status','=',3);
+            $responds = $this->dating($body,$answers);
+            $res['not_respond'] = count($responds);
+        }
+        foreach($answers as $answer) {
+            $res['body'][] = $answer;
+        }
+
+        echo json_encode($res);
+    }
+
+    private function dating($body,$answers)  {
         if($body['start_date'] != null) {
             //tenemos fecha inicio
             if($body['end_date'] != null) {
                 //tenemos fechas
-                $answer->whereBetween('expiration',[$body['start_date'],$body['end_date']]);
+                $answers->whereBetween('expiration',[$body['start_date'],$body['end_date']]);
             } else {
                 //hasta fecha actual
-                $answer->where('expiration','<=',$body['start_date']);
+                $answers->where('expiration','<=',$body['start_date']);
             }
         } else {
             if($body['end_date'] != null) {
                 //tenemos fecha final
-                $answer->where('expiration','>=',$body['end_date']);
+                $answers->where('expiration','>=',$body['end_date']);
             }
         }
 
-        $answer->get();
-
-        $response = [];
-        //$response['total'] = count($answer);
-        $response['body'] = $answer;
-
-        echo json_encode($response);
+        return $answers;
     }
 }
