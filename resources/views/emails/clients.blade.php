d5ef87a89fb89f2d95076d7658c0f95391e60633
Daniel Molina <daniel.molina@optimaretail.es>
2022-07-05 17:43:41 +0200

    button link text

--- resources/views/emails/clients.blade.php
+++ resources/views/emails/clients.blade.php
@@ -134,20 +134,6 @@ switch($body['month']) {
                     <div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
                       <!--<![endif]-->
 
-                      <table style="font-family:'Poppins',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:'Poppins',sans-serif;" align="left">
-
-                              <div style="color: #afb0c7; line-height: 170%; text-align: center; word-wrap: break-word;">
-                                <p style="font-size: 14px; line-height: 170%;"><span style="font-size: 14px; line-height: 23.8px;">View Email in Browser</span></p>
-                              </div>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
                       <!--[if (!mso)&(!IE)]><!-->
                     </div>
                     <!--<![endif]-->
@@ -346,14 +332,16 @@ switch($body['month']) {
 
                               <div align="center">
                                 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;font-family:'Poppins',sans-serif;"><tr><td style="font-family:'Poppins',sans-serif;" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="" style="height:46px; v-text-anchor:middle; width:234px;" arcsize="8.5%" stroke="f" fillcolor="#ff6600"><w:anchorlock/><center style="color:#FFFFFF;font-family:'Poppins',sans-serif;"><![endif]-->
-                                <a href="https://optimaquality.es/public/{{$body['id']}}" target="_blank" style="box-sizing: border-box;display: inline-block;font-family:'Poppins',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #ff6600; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;">
+                                <a href="https://optimaquality.es/public/{{$body['id']}}" style="box-sizing: border-box;display: inline-block;font-family:'Poppins',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #ff6600; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;">
                                   <span style="display:block;padding:14px 44px 13px;line-height:120%;"><span style="font-size: 16px; line-height: 19.2px;"><strong><span style="line-height: 19.2px; font-size: 16px;">{{__('MORE INFORMATION')}}</span></strong>
                                   </span>
                                   </span>
                                 </a>
                                 <!--[if mso]></center></v:roundrect></td></tr></table><![endif]-->
                               </div>
-
+                              <div alig="center">
+                                {{__('button link: ')}} https://optimaquality.es/public/{{$body['id']}}
+                              </div>
                             </td>
                           </tr>
                         </tbody>
