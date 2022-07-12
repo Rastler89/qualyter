2ef52cc24f6af3a0d447a4a32b2fb71365493692
Daniel Molina <daniel.molina@optimaretail.es>
2022-07-12 11:14:30 +0200

    process

--- resources/views/admin/answer/index.blade.php
+++ resources/views/admin/answer/index.blade.php
@@ -241,7 +241,7 @@
     </div>
     @elseif($answer->status != 3)
     <form method="POST" action="{{route('answers.send', ['id'=>$answer->id])}}">
-        @csrf;
+        @csrf
     <div class="modal fade" id="technician{{$answer->id}}" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
