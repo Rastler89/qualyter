2fd1002d5552d15b8e58c297215d9a7c40491c93
Daniel Molina <daniel.molina@optimaretail.es>
2022-07-13 15:43:50 +0200

    process

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -38,21 +38,21 @@ class ApiController extends Controller
         $first_month_pre = $this->first_month_day(1);
         $last_month_pre = $this->last_month_day(1);
 
-        $finish_today = count(Answer::whereIn('status',[2,4,5])->where('expiration',[$first_month,$last_month])->get());
-        $fisnih_yesterday = count(Answer::whereIn('status',[2,4,5])->where('expiration',[$first_month_pre,$last_month_pre])->get());
+        $finish_today = count(Answer::whereIn('status',[2,4,5])->whereBetween('expiration',[$first_month,$last_month])->get());
+        $fisnih_yesterday = count(Answer::whereIn('status',[2,4,5])->whereBetween('expiration',[$first_month_pre,$last_month_pre])->get());
         $porcentaje_finalizadas = number_format(($finish_today/$fisnih_yesterday)*100-100,2);
 
         $response['finish'] = $finish_today;
         $response['porcentage'] = $porcentaje_finalizadas;
 
-        $total_today = count(Answer::where('expiration',[$first_month,$last_month])->where('status','<>','8')->get());
+        $total_today = count(Answer::whereBetween('expiration',[$first_month,$last_month])->where('status','<>','8')->get());
         $porcentaje_dia = number_format(($finish_today/$total_today)*100,2);
 
         $response['total'] = $total_today;
         $response['complete'] = $porcentaje_dia;
 
-        $total_cancelled = count(Answer::where('expiration','=',date('Y-m-d'))->where('status','=','8')->get());
-        $total_cancelled_yesterday = count(Answer::where('status','=','8')->where('expiration',[$first_month_pre,$last_month_pre])->get());
+        $total_cancelled = count(Answer::whereBetween('expiration',[$first_month,$last_month])->where('status','=','8')->get());
+        $total_cancelled_yesterday = count(Answer::where('status','=','8')->whereBetween('expiration',[$first_month_pre,$last_month_pre])->get());
 
         $response['cancelled'] = $total_cancelled;
         $response['cancelled_yesterday'] = $total_cancelled_yesterday;
