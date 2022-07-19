2a9241b98d1d858c144933bc722bbcf7d2a59646
Daniel Molina <daniel.molina@optimaretail.es>
2022-07-19 13:00:29 +0200

    up version

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row text-muted">
             <div class="col-6 text-start">
                 <p class="mb-0">
-                    <a class="text-muted" href="https://optimaretail.es/" target="_blank"><strong>OptimaRetail</strong></a> &copy; - Qualyter v1.1.0
+                    <a class="text-muted" href="https://optimaretail.es/" target="_blank"><strong>OptimaRetail</strong></a> &copy; - Qualyter v1.2.0
                 </p>
             </div>
         </div>
