c57e7d11d6a15861c6c28bb97ed0454f2c6f9d4b
Daniel Molina <daniel.molina@optimaretail.es>
2022-09-22 09:58:26 +0200

    solved

--- app/Http/Controllers/IncidenceController.php
+++ app/Http/Controllers/IncidenceController.php
@@ -427,10 +427,10 @@ class IncidenceController extends Controller
             $agent = Agent::find($incidence->owner);
             $user = User::find($incidence->responsable);
             Mail::to($agent['email'])->send(new NotifyMail($body));
-            if($manager->id!=auth()->uswer()->id) {
+            if($manager->id!=auth()->user()->id) {
                 Mail::to($manager->email)->send(new ManagerMail($body));
             }
-            if($user['id']!=auth()->uswer()->id) {
+            if($user['id']!=auth()->user()->id) {
                 Mail::to($user['email'])->send(new ManagerMail($body));
             }
         }
